4f039c0f1be7dd06829a3facc066f27ad29167fb
seoh7dev <seoh7dev@gmail.com>
2024-11-05 10:51:00 +0900

    fix: insect @Repository annotation 추가

--- backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/repository/InsectRepository.java
+++ backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/repository/InsectRepository.java
@@ -1,9 +1,10 @@
 package com.ssafy.bugar.domain.insect.repository;
 
 import com.ssafy.bugar.domain.insect.entity.Insect;
-import com.ssafy.bugar.domain.insect.entity.RaisingInsect;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface InsectRepository extends JpaRepository<Insect, Long> {
 
     Insect findByInsectId(Long insectId);
