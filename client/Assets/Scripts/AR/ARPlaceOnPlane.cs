7b7d72b0ad42ffee0563f2e0a8edaceba527e576
seojiheun <wlgmsaudqls@naver.com>
2024-11-05 20:32:25 +0900

    feat: Insect 밥 점수 증가

--- client/Assets/Scripts/AR/ARPlaceOnPlane.cs
+++ client/Assets/Scripts/AR/ARPlaceOnPlane.cs
@@ -112,7 +112,20 @@ public class ARPlaceOnPlane : MonoBehaviour
             isInsectMoving = false; // 이동 중지
 
             SetInsectIdle();
+
+            var increaseScoreRequest = new Models.Insect.Request.IncreaseScore
+            {
+                raisingInsectId = insectInfo.insectId,
+                category = 1
+            };
             Debug.Log("지흔: Food를 먹었습니다!");
+
+            StartCoroutine(insectApi.PostIncreaseScore(increaseScoreRequest,
+                onSuccess: () => Debug.Log("점수 증가 성공"),
+                onFailure: error => Debug.LogError("점수 증가 실패: " + error)
+            ));
+
+            //여기에 그 api 함수 불러
             Destroy(foodObject);
             foodObject = null;
         }
