bea9ba9cbc761f451b97cb3f62c2edfbe0ffac93
seoh7dev <seoh7dev@gmail.com>
2024-11-06 14:31:01 +0900

    fix: insect 지역별 곤충 여부 확인 API response 수정

--- backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/repository/RaisingInsectRepository.java
+++ backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/repository/RaisingInsectRepository.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Repository;
 public interface RaisingInsectRepository extends JpaRepository<RaisingInsect, Long> {
 
     @Query(value = """
-            SELECT ri.raising_insect_id AS raisingInsectId, ri.insect_id AS insectId, ri.insect_nickname AS nickname, i.family AS family
+            SELECT ri.raising_insect_id AS raisingInsectId, ri.insect_nickname AS nickname, i.family AS family
             FROM raising_insects ri
             JOIN insects i ON ri.insect_id = i.insect_id
             JOIN area a ON i.area_id = a.area_id
