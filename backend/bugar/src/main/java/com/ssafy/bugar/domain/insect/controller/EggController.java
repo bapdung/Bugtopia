69cd8cde46e19ee7472253f28cbf3663fdede2e0
kiminchae <kiminchae@naver.com>
2024-11-07 16:30:18 +0900

    feat: 알 관련 API 생성

--- backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/controller/EggController.java
+++ backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/controller/EggController.java
@@ -3,6 +3,8 @@ package com.ssafy.bugar.domain.insect.controller;
 import com.ssafy.bugar.domain.insect.dto.request.EggRaiseRequestDto;
 import com.ssafy.bugar.domain.insect.dto.response.SaveRaisingInsectResponseDto;
 import com.ssafy.bugar.domain.insect.service.EggService;
+import com.ssafy.bugar.domain.notification.dto.response.NotificationResponseDto;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -21,11 +23,17 @@ public class EggController {
 
     private final EggService eggService;
 
-    @PostMapping("/egg/{eggId}")
+    @PostMapping("/receive/{eggId}")
     public ResponseEntity<SaveRaisingInsectResponseDto> receiveEgg(@RequestHeader("userId") Long userId,
                                                                    @PathVariable Long eggId,
                                                                    @RequestBody EggRaiseRequestDto request) {
         SaveRaisingInsectResponseDto response = eggService.raise(eggId, userId, request.getNickname());
+        return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/{raisingInsectId}")
+    public ResponseEntity<NotificationResponseDto> createEgg(@PathVariable Long raisingInsectId) throws IOException {
+        NotificationResponseDto response = eggService.save(raisingInsectId);
         return ResponseEntity.ok(response);
     }
 }
--- backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/service/EggService.java
+++ backend/bugar/src/main/java/com/ssafy/bugar/domain/insect/service/EggService.java
@@ -6,9 +6,14 @@ import com.ssafy.bugar.domain.insect.entity.RaisingInsect;
 import com.ssafy.bugar.domain.insect.enums.RaiseState;
 import com.ssafy.bugar.domain.insect.repository.EggRepository;
 import com.ssafy.bugar.domain.insect.repository.RaisingInsectRepository;
+import com.ssafy.bugar.domain.notification.dto.response.NotificationResponseDto;
 import com.ssafy.bugar.domain.notification.enums.NotificationType;
+import com.ssafy.bugar.domain.notification.service.NotificationService;
+import com.ssafy.bugar.global.exception.CustomException;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,25 +25,27 @@ public class EggService {
     private final EggRepository eggRepository;
     private final RaisingInsectRepository raisingInsectRepository;
     private final RaisingInsectService raisingInsectService;
+    private final NotificationService notificationService;
 
+    // 알 생성
     @Transactional
-    public void save(Long raisingInsectId, NotificationType type) {
-        if (!validate(raisingInsectId, type)) {
-            return;
+    public NotificationResponseDto save(Long raisingInsectId) throws IOException {
+        if (!validate(raisingInsectId)) {
+            throw new CustomException(HttpStatus.BAD_REQUEST, "알을 낳을 수 있는 곤충인지 다시 확인해주세요.");
         }
         RaisingInsect raisingInsect = raisingInsectRepository.findById(raisingInsectId).orElseThrow();
         Egg egg = new Egg(raisingInsect.getInsectNickname(), raisingInsect.getInsectId(), raisingInsect.getUserId());
         eggRepository.save(egg);
+        return notificationService.save(raisingInsectId, NotificationType.BABY);
     }
 
-    public Boolean validate(Long raisingInsectId, NotificationType type) {
-        if (type != NotificationType.BABY) {
-            return false;
-        }
+    // 알 낳을 수 있는 곤충인지 판별
+    public Boolean validate(Long raisingInsectId) {
         RaisingInsect insect = raisingInsectRepository.findByRaisingInsectId(raisingInsectId);
         return insect.getState() == RaiseState.DONE;
     }
 
+    // 알 -> 키우기
     @Transactional
     public SaveRaisingInsectResponseDto raise(Long eggId, Long userId, String nickname) {
         Egg egg = eggRepository.findByEggId(eggId);
