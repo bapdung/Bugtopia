05f22c8b355d687ff78ec0a1b8ed42e08ef1feb0
JHS <ghtjd1551@naver.com>
2024-11-07 12:40:24 +0900

    feat: camera API

---
+++ client/Assets/Scripts/API/CatchApi.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+using System.Text;
+
+namespace API.Catch
+{
+    public class CatchApi : MonoBehaviour
+    {
+        [SerializeField] private EnvironmentConfig environmentConfig;
+
+        private string catchUrl;
+
+        void Awake()
+        {
+            if (environmentConfig == null)
+            {
+                environmentConfig = Resources.Load<EnvironmentConfig>("EnvironmentConfig");
+            }
+            catchUrl = $"{environmentConfig.baseUrl}/catch";
+        }
+
+        // S3 URL을 얻는 메서드
+        public IEnumerator GetS3Url(string fileName, byte[] photoBytes)
+        {
+            string requestUrl = $"{environmentConfig.baseUrl}/files/upload/{fileName}";
+            string responseS3Url = string.Empty;
+
+            using (UnityWebRequest request = UnityWebRequest.Get(requestUrl))
+            {
+                yield return request.SendWebRequest();
+
+                if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.LogError(request.error);
+                }
+                else
+                {
+                    var jsonResponse = JsonUtility.FromJson<S3Response>(request.downloadHandler.text);
+                    responseS3Url = jsonResponse.data.path;
+                    Debug.Log(responseS3Url);
+                }
+            }
+
+            if (!string.IsNullOrEmpty(responseS3Url))
+            {
+                StartCoroutine(UploadPhotoToS3(responseS3Url, fileName, photoBytes));
+            }
+        }
+
+        // S3에 사진을 업로드하는 메서드
+        public IEnumerator UploadPhotoToS3(string s3Url, string fileName, byte[] photoBytes)
+        {
+            UnityWebRequest request = new UnityWebRequest(s3Url, "PUT");
+            request.uploadHandler = new UploadHandlerRaw(photoBytes);
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "image/png");
+
+            yield return request.SendWebRequest();
+
+            if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError(request.error);
+            }
+            else
+            {
+                Debug.Log("s3에 사진 업로드 성공");
+                string photoUrl = s3Url; 
+                StartCoroutine(PostSearchInsect(photoUrl));
+            }
+        }
+
+        // 'postSearchInsect' API에 사진 URL을 전송
+        public IEnumerator PostSearchInsect(string photoUrl)
+        {
+            string requestUrl = $"{catchUrl}/search";
+
+            var searchRequest = new SearchInsectRequest
+            {
+                photoUrl = photoUrl
+            };
+
+            string jsonRequest = JsonUtility.ToJson(searchRequest);
+
+            using (UnityWebRequest request = new UnityWebRequest(requestUrl, "POST"))
+            {
+                byte[] jsonBytes = Encoding.UTF8.GetBytes(jsonRequest);
+                request.uploadHandler = new UploadHandlerRaw(jsonBytes);
+                request.downloadHandler = new DownloadHandlerBuffer();
+                request.SetRequestHeader("Content-Type", "application/json");
+
+                yield return request.SendWebRequest();
+
+                if (request.result == UnityWebRequest.Result.ConnectionError || request.result == UnityWebRequest.Result.ProtocolError)
+                {
+                    Debug.LogError(request.error);
+                }
+                else
+                {
+                    Debug.Log(request.downloadHandler.text);  
+                }
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class S3Response
+    {
+        public S3Data data;
+    }
+
+    [System.Serializable]
+    public class S3Data
+    {
+        public string path;
+    }
+
+    [System.Serializable]
+    public class SearchInsectRequest
+    {
+        public string photoUrl;
+    }
+}
--- client/Assets/Scripts/Catch/CameraManager.cs
+++ client/Assets/Scripts/Catch/CameraManager.cs
@@ -4,35 +4,67 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Android;
 using UnityEngine.UI;
+using API.Catch;
 
 public class CameraManager : MonoBehaviour
 {
     public RawImage cameraView;
     private WebCamTexture webCamTexture;
+    public GameObject HelpPanel;
+    private CatchApi catchApi;
 
     void Start()
     {
+        // 권한 받기
+        if (!Permission.HasUserAuthorizedPermission(Permission.Camera))
+        {
+            Permission.RequestUserPermission(Permission.Camera);
+        }
+
         if (WebCamTexture.devices.Length > 0) 
         {
+            Debug.Log("카메라 디바이스 감지됨: " + WebCamTexture.devices[0].name);
             webCamTexture = new WebCamTexture(WebCamTexture.devices[0].name);
             cameraView.texture = webCamTexture;
             webCamTexture.Play();
         }
+
+        if (HelpPanel != null)
+        {
+            HelpPanel.SetActive(false); // 도움말 숨기고 시작
+            Debug.Log("판넬 찾고 숨겻음");
+        }
+
+        catchApi = FindObjectOfType<CatchApi>();
     }
 
-    public void TakePhoto()
+    public void TakePhotoButton()
     {
         Texture2D photo = new Texture2D(webCamTexture.width, webCamTexture.height);
         photo.SetPixels(webCamTexture.GetPixels());
         photo.Apply();
 
-        // AI 모델에 사진을 전송하는 코드 추가 필요
+        string fileName = "photo_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+        byte[] photoBytes = photo.EncodeToPNG(); 
 
-        SceneManager.LoadScene("ResultScene");  // 결과 화면으로 이동
+        // S3 URL 요청 및 사진 업로드
+        StartCoroutine(catchApi.GetS3Url(fileName, photoBytes));
+
+        SceneManager.LoadScene("LoadingScene"); 
     }
 
-    public void BackToHome()
+    public void BackToHomeButton()
     {
         SceneManager.LoadScene("MainScene");  
+    }
+
+    public void HelpButton()
+    {
+        Debug.Log("버튼 클릭됨");
+        // Help Panel의 활성화 상태 반전
+        if (HelpPanel != null)
+        {
+            HelpPanel.SetActive(!HelpPanel.activeSelf);
+        }
     }
 }
